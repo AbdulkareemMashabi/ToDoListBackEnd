21fb53f3260385f36976e117be525c6305c59b87
AbdulkareemMashabi <abdulkareem_mashabi@hotmail.com>
2025-05-30 16:49:37 +0300

    feat: adding routes

--- routes/task.js
+++ routes/task.js
@@ -7,5 +7,9 @@ const router = express.Router();
 
 router.get("/list", isAuth, tasksController.getAllTasks);
 router.post("/create-task", isAuth, tasksController.createTask);
+router.put("/update-task", isAuth, tasksController.updateTask);
+router.put("/update-status", isAuth, tasksController.updateTaskStatus);
+router.put("/update-favorite", isAuth, tasksController.updateFavorite);
+router.delete("/delete-task", isAuth, tasksController.deleteTask);
 
 module.exports = router;
